3c8fa1d1775df4542b934d902269c024f0b7574d
johnnyman727 <johnnyman727@gmail.com>
2014-05-09 18:34:57 -0700

    Added lua bindings to audio shim

--- src/module_shims/audio-vs1053b.c
+++ src/module_shims/audio-vs1053b.c
@@ -1,11 +1,12 @@
 #include "audio-vs1053b.h"
 
-void audio_play_buffer(uint8_t cs, uint8_t dreq, uint8_t buf) {
+int audio_play_buffer(uint8_t cs, uint8_t dreq, const uint8_t *buf, uint32_t buf_len) {
   cs++;
   dreq++;
   buf++;
+  buf_len++;  
 
-  // Initialize SPI
+  // Initialize SPI (I guess we can do this outside of C, in JS)
 
   // while there are still bytes to transfer
     // wait for dreq to go low
@@ -13,4 +14,5 @@ void audio_play_buffer(uint8_t cs, uint8_t dreq, uint8_t buf) {
       // transfer 32 bytes
         // wait for transfer to complete.
           // Pull CS high
+  return 17;
 }
--- src/module_shims/audio-vs1053b.h
+++ src/module_shims/audio-vs1053b.h
@@ -1,4 +1,4 @@
 #include "tm.h"
 #include "hw.h"
 
-void audio_play_buffer(uint8_t cs, uint8_t dreq, uint8_t buf);
+int audio_play_buffer(uint8_t cs, uint8_t dreq, const uint8_t *buf, uint32_t buf_len);
