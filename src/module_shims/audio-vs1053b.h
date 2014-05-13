7a424ab3f3e47e879785742df4ba0c3e4d73adff
johnnyman727 <johnnyman727@gmail.com>
2014-05-12 22:33:48 -0700

    Adding first draft of audio shim transfer

--- src/module_shims/audio-vs1053b.h
+++ src/module_shims/audio-vs1053b.h
@@ -1,4 +1,6 @@
 #include "tm.h"
 #include "hw.h"
+#include "string.h"
+#include "colony.h"
 
-int audio_play_buffer(uint8_t cs, uint8_t dreq, const uint8_t *buf, uint32_t buf_len);
+int audio_play_buffer(uint8_t chip_select, uint8_t dreq, const uint8_t *buf, uint32_t buf_len);
