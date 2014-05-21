f4bd127f38aebe9e13d23d8ef536503adc33cc29
johnnyman727 <johnnyman727@gmail.com>
2014-05-20 22:15:26 -0700

    Updates function names to account for changed lua to_buffer func

--- src/module_shims/audio-vs1053b.h
+++ src/module_shims/audio-vs1053b.h
@@ -72,7 +72,7 @@ uint8_t audio_get_state();
 // Load a plugin and start recording sound
 int8_t audio_start_recording(uint8_t command_select, uint8_t dreq, const char *plugin_dir, uint8_t *fill_buf, uint32_t fill_buf_len, uint32_t buf_ref, uint8_t mic_input);
 // Stop recording sound
-int8_t audio_stop_recording(bool flush);
+int8_t audio_stop_recording();
 
 // Stop all playback and recording (used after ctrl+c)
 void audio_reset();
